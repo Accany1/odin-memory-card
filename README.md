b43a390c21b5154f644cbf93b261ef5f4d110217
Accany1 <129660499+Accany1@users.noreply.github.com>
2025-02-18 20:22:03 +0800

    working card randomizer, score counter, and best score

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ https://www.theodinproject.com/lessons/node-path-react-new-memory-card
 
 # Todo
 - [ ] Score counter
-- [ ] Pokemon API fetching from https://pokeapi.co/
+- [x] Pokemon API fetching from https://pokeapi.co/
 - [ ] Card Formatting
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState, useRef } from 'react'
 import './App.css'
 
 const MAX_POKEMON = 1025
 const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon/"
 
-function PokemonCard({name, image}) {
+function PokemonCard({name, image,onClick}) {
   return (
-    <div className="card">
-      <img src={image} alt={name} />
+    <div onClick={onClick} className="card">
+      <img src={image} alt={name} width={250} height={250} />
       <div className="name">{name}</div>
     </div>
   )
@@ -18,6 +16,9 @@ function PokemonCard({name, image}) {
 function App() {
   const [count, setCount] = useState(0)
   const [pokemon, setPokemon] = useState([])
+  const [restart, setRestart] = useState(false)
+  const [selected, setSelected] = useState([])
+  const [bestScore, setBestScore] = useState(0)
 
   function randomID() {
     return Math.floor(Math.random() * MAX_POKEMON)
@@ -33,17 +34,39 @@ function App() {
     let mounted = true
 
     if (mounted) {
-      for (let i = 0; i < 4; i++) {
+      for (let i = 0; i < 6; i++) {
         fetchPokemon()
       }
     }
     
     return () => {
       mounted = false
-    }},[])
+    }},[]
+  )
 
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array
+  }
 
-  
+  const handleClick = (name) => {
+    if (selected.includes(name)) {
+      setSelected([])
+      setRestart(true)
+      if (count > bestScore) {
+        setBestScore(count)
+      }
+      setCount(0)
+      setPokemon(shuffleArray(pokemon))
+    } else {
+      setSelected([...selected, name])
+      setCount(count + 1)
+      setPokemon(shuffleArray(pokemon))
+    }
+  }
 
   return (
     <>
@@ -51,10 +74,10 @@ function App() {
         <div className="title">
           <h1>Pokemon Memory Game</h1>
           <div>Score: {count}</div>
-          <div>Best Score: 0</div>
+          <div>Best Score: {bestScore}</div>
         </div>
-        <div className="card-holder">
-          {pokemon.map((p) => {return <PokemonCard key={p.id} name={p.name} image={p.sprites.front_default} />})}
+        <div className="card-container">
+          {pokemon.map((p) => {return <PokemonCard key={p.id} name={p.name} image={p.sprites.other['official-artwork'].front_default} onClick={(e) => handleClick(p.name)} />})}
         </div>
       </div>
     </>
